97a7be72d31ee976addf309708bf49f7ed9f7849
Charlesrodil <crodil1@nait.ca>
2023-09-11 21:42:12 -0600

    Created Position enum, Shot enum, fields and properties for Hockeyplayer class, and def. + greedy constructor

--- CPSC1517Workbook/HockeyData/HockeyPlayer.cs
+++ CPSC1517Workbook/HockeyData/HockeyPlayer.cs
@@ -1,9 +1,109 @@
-﻿namespace HockeyData
+﻿using Hockey.Data;
+using System.Security.AccessControl;
+
+namespace Hockey.Data
 {
+    // <summary>
+    // An instance of this class will hold data about a hockey player
+    // </summary>
+    
     public class HockeyPlayer
     {
+        // Data fields
+
+        private string _birthplace;
+        private string _firstName;
+        private string _lastName;
+        private int _heightInInches;
+        private int _weightInPounds;
+        private DateOnly _dateOfBirth;
         
 
+        //Properties
+
+        public string BirthPlace 
+        { 
+            get 
+            { 
+                return _birthplace; 
+            }
+            
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Birth place cannot be null or empty.");
+                }
+
+                _birthplace = value;
+                
+            }
+            
+        }
+
+        public int HeightInInches
+        {
+            get
+            {
+                return _heightInInches;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height must be positive.");
+                }
+
+                _heightInInches = value;
+
+            }
+
+        }
+
+        public DateOnly DateOfBirth
+        {
+            get
+            {
+                return _dateOfBirth;
+            }
+
+            set
+            {
+                // TODO: update validation
+                _dateOfBirth = value;
+            }
+        }
+
+        public Position Position { get; set; }
+
+        public Shot Shot { get; set; }
+
+        // Default constructor
+        public HockeyPlayer()
+        { 
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _birthplace= string.Empty;
+            _dateOfBirth = new DateOnly();
+            _weightInPounds = 0;
+            _heightInInches= 0;
+            Position = Position.Center;
+            Shot = Shot.Left;
+        }
+
+        //Greedy constructor
+
+        public HockeyPlayer(string firstName, string lastName, string birthPlace, DateOnly birthDate, int weightInPounds, int heightInInches, Position position, Shot shot)
+        {
+            BirthPlace = birthPlace;
+            HeightInInches = heightInInches;
+            DateOfBirth = birthDate;
+            Position = position;
+            Shot = shot;
+        }
+
+        // HockeyPlayer player = new HockeyPlayer("jane", "doe", "edmonton", newDateOnly(), 1, 2);
 
     }
 }
